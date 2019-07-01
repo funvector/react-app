7519aa11c120eadc9ccba450104c1e2713039c2c
Vlad Kozlovskii <vkozlovsky77@gmail.com>
2019-07-01 17:47:59 +0300

    add: ref version of redux counter

---
+++ my-app/counter.js
@@ -0,0 +1,20 @@
+export default function  createStore(reducer, initialState) {
+  let state = initialState;
+  let callbacks = [];
+
+  const getState = () => state;
+  
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    callbacks.forEach((callback) => callback());
+  ;}
+
+  const subscribe = (callback) => {
+    callbacks.push(callback);
+    return () => callbacks.filter((cb) => cb !== callback)
+  }
+
+  return {
+    getState, dispatch, subscribe
+  }
+}
--- my-app/my-redux.js
+++ my-app/my-redux.js
@@ -1,141 +1,208 @@
-const initialState = {};
+// const initialState = {};
 
-function createStore(reducer, state = initialState) {
-  return {
-      dispatch: action => { state = reducer(state, action) },
-      getState: () => state,
-  }
+// function createStore(reducer, state = initialState) {
+//   return {
+//       dispatch: action => { state = reducer(state, action) },
+//       getState: () => state,
+//   }
+// }
+
+// // rootReducer
+// function rootReducer(state, action) {
+//   switch (action.type) {
+//     case 'smth':
+//       return [
+//         ...state, 
+//         {...action.text}
+//     ]
+//     default:
+//       return state
+//   }
+// }
+
+// const store = createStore(rootReducer);
+
+// store.dispatch({
+//   type: 'smth',
+//   text: 'some txt'
+// })
+
+// store.getState()
+
+// // getState
+
+// let isDispatching = true;
+// let currentState = initialState;
+// let currentListeners = [];
+// let nextListeners = currentListeners;
+
+// function getState() {
+//   if (isDispatching) {
+//     throw new Error(
+//       'some txt'
+//     )
+//   }
+
+//   return currentState;
+// }
+
+// // subscribe\unsubscribe это уже из оф доков...не знаю как переписать это лучше чем тут написано)
+// function ensureCanMutateNextListeners() {
+//   if (nextListeners === currentListeners) {
+//     nextListeners = currentListeners.slice()
+//   }
+// }
+
+// function subscribe(listener) {
+
+//   if (isDispatching) {
+//     throw new Error(
+//       'some txt'
+//     )
+//   }
+
+//   let isSubscribed = true
+
+//   ensureCanMutateNextListeners()
+//   nextListeners.push(listener)
+
+//   return function unsubscribe() {
+//     if (!isSubscribed) {
+//       return
+//     }
+
+//     if (isDispatching) {
+//       throw new Error(
+//         'some txt'
+//       )
+//     }
+
+//     isSubscribed = false
+
+//     ensureCanMutateNextListeners()
+//     const index = nextListeners.indexOf(listener)
+//     nextListeners.splice(index, 1)
+//   }
+// }
+
+// // observable
+// function observable() {
+//   const outerSubscribe = subscribe;
+//   return {
+//     subscribe(observer) {
+//       if (typeof observer !== 'object' || observer === null) {
+//         throw new TypeError('Expected the observer to be an object.')
+//       }
+
+//       function observeState() {
+//         if (observer.next) {
+//           observer.next(getState())
+//         }
+//       }
+
+//       observeState()
+//       const unsubscribe = outerSubscribe(observeState)
+//       return { unsubscribe }
+//     },
+
+//     [$$observable]() {
+//       return this
+//     }
+//   }
+// }
+
+// // combineReducers
+// function combineReducers(reducersMap) {
+//   return function combinationReducer(state, action) {
+//     const nextState = {}
+//     Object.entries(reducersMap).forEach(([key, reducer]) => {
+//       nextState[key] = reducer(state[key], action)
+//     })
+//     return nextState
+//   }
+// }
+
+// // Disppatcher..его проще описать классом) выглядит симпотичнее
+
+// class Disppatcher{
+//   constructor(store = { getState: () => {}, dispatch: () => {} }) {
+//     this.store = store;
+//   }
+
+//   // type - действия пользователя, payload - какая то структура данных, date - доп данные, если нужны, err - проверка на успешность...true\false
+//   createAction(type, payload, date, err) {
+//     return (type, payload, date, err);
+//   }
+
+//   dispatchAction(type, payload, date, err) {
+//     const { store } = this;
+//     store.dispatch(this.createAction(type, payload, date, err))
+//   }
+// }
+
+// !
+
+import React, { Fragment, Component } from 'react';
+import ReactDOM from 'react-dom'
+import createStore from './counter';
+
+const initialState = {
+  count: 0
 }
 
-// rootReducer
-function rootReducer(state, action) {
+function reducer(state = initialState, action){
   switch (action.type) {
-    case 'smth':
-      return [
-        ...state, 
-        {...action.text}
-    ]
+    case 'INCREMENT':
+      return {count: state.count + 1};
+    case 'DECREMENT':
+      return {count: state.count - 1};
     default:
-      return state
+      return state;
   }
-}
+};
 
-const store = createStore(rootReducer);
+const INCREMENT = {type: 'INCREMENT'};
+const DECREMENT = {type: 'DECREMENT'};
 
-store.dispatch({
-  type: 'smth',
-  text: 'some txt'
-})
+const store = createStore(reducer, initialState);
 
-store.getState()
+class Caunter extends Component {
+  constructor(props){
+    super(props);
+    this._increment = this._increment.bind(this);
+    this._decrement = this._decrement.bind(this);
+  }
 
-// getState
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate())
+  }
 
-let isDispatching = true;
-let currentState = initialState;
-let currentListeners = [];
-let nextListeners = currentListeners;
+  increment(){
+    store.dispatch(this.INCREMENT)
+  }
 
-function getState() {
-  if (isDispatching) {
-    throw new Error(
-      'some txt'
+  decrement(){
+    store.dispatch(this.DECREMENT)
+  }
+  
+  render() {
+    const count = store.getState().count;
+    return (
+      <Fragment>
+        <span>
+          {count}
+        </span>
+        <div>
+          <button onClick={this.decrement}>
+            -
+          </button>
+          <button onClick={this.increment}>
+            +
+          </button>
+        </div>
+      </Fragment>
     )
   }
-
-  return currentState;
 }
 
-// subscribe\unsubscribe это уже из оф доков...не знаю как переписать это лучше чем тут написано)
-function ensureCanMutateNextListeners() {
-  if (nextListeners === currentListeners) {
-    nextListeners = currentListeners.slice()
-  }
-}
-
-function subscribe(listener) {
-
-  if (isDispatching) {
-    throw new Error(
-      'some txt'
-    )
-  }
-
-  let isSubscribed = true
-
-  ensureCanMutateNextListeners()
-  nextListeners.push(listener)
-
-  return function unsubscribe() {
-    if (!isSubscribed) {
-      return
-    }
-
-    if (isDispatching) {
-      throw new Error(
-        'some txt'
-      )
-    }
-
-    isSubscribed = false
-
-    ensureCanMutateNextListeners()
-    const index = nextListeners.indexOf(listener)
-    nextListeners.splice(index, 1)
-  }
-}
-
-// observable
-function observable() {
-  const outerSubscribe = subscribe;
-  return {
-    subscribe(observer) {
-      if (typeof observer !== 'object' || observer === null) {
-        throw new TypeError('Expected the observer to be an object.')
-      }
-
-      function observeState() {
-        if (observer.next) {
-          observer.next(getState())
-        }
-      }
-
-      observeState()
-      const unsubscribe = outerSubscribe(observeState)
-      return { unsubscribe }
-    },
-
-    [$$observable]() {
-      return this
-    }
-  }
-}
-
-// combineReducers
-function combineReducers(reducersMap) {
-  return function combinationReducer(state, action) {
-    const nextState = {}
-    Object.entries(reducersMap).forEach(([key, reducer]) => {
-      nextState[key] = reducer(state[key], action)
-    })
-    return nextState
-  }
-}
-
-// Disppatcher..его проще описать классом) выглядит симпотичнее
-
-class Disppatcher{
-  constructor(store = { getState: () => {}, dispatch: () => {} }) {
-    this.store = store;
-  }
-
-  // type - действия пользователя, payload - какая то структура данных, date - доп данные, если нужны, err - проверка на успешность...true\false
-  createAction(type, payload, date, err) {
-    return (type, payload, date, err);
-  }
-
-  dispatchAction(type, payload, date, err) {
-    const { store } = this;
-    store.dispatch(this.createAction(type, payload, date, err))
-  }
-}
+ReactDOM.render(<Caunter />, document.getElementById('root'));
