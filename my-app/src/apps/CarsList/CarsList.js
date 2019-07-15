5efd605c067be3091afaceef26e59f0f515ae08e
Vlad Kozlovskii <vkozlovsky77@gmail.com>
2019-07-15 20:43:39 +0300

    feat: fix some code

--- my-app/src/apps/CarsList/CarsList.js
+++ my-app/src/apps/CarsList/CarsList.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import classNames from 'classnames';
-import { isMarked } from './../../redux-app/selectors'
 
 export default class CarsList extends React.Component{
   
   render() {
 
-    let cardClassMarked = classNames('card effect8', this.props.className, {
-      'card-marked': isMarked,
-      'card effect8': !isMarked
-    });
+    return this.props.cars.map((car) => {
 
-    return this.props.cars.map((car) => (
+      let cardClassMarked = classNames('card effect8', this.props.className, {
+        'card-marked': car.marked
+      });
+      
+      return (
         <div className={cardClassMarked} key={car.id} onClick={() => this.props.handleMarked(car.id)}>
           <div className='card-img'>
             <img 
-              src={car.img} 
+              src={car.img}
               alt={car.model}
             />
             </div>
             <h3>{car.model}</h3>
             <p>{car.price} $</p>
           </div>
-        )
+        )}
       )
     }
   }
