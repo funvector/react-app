ddf80e175500d3a050268460b00b57b1106002c2
Vlad K <huza007@mail.com>
2019-11-18 04:30:17 +0300

    feat: create build v

---
+++ my-app/build/precache-manifest.5d6a76d645fcadc96e7df36d85b685f8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95b48358e1d4b8a81fc1143e0a7adeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0150ad64e94a6a65b3c",
+    "url": "/static/js/2.fbd4e3da.chunk.js"
+  },
+  {
+    "revision": "d86e6477319ec6eba94d",
+    "url": "/static/js/main.cad227ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
